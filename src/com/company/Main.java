5b27163322300adee1bee99b84c3bc26d0542725
p-ganesh <pg304@pvsd.org>
2018-12-10 11:21:48 -0500

    huh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	    Scanner kb = new Scanner(System.in);
+        FileWriter fw = new FileWriter("C:\\")
+        PrintWriter file = new PrintWriter("TruthTable.txt");
+
+        System.out.println("Enter the expression you would like a truth table for.");
+        String exp = kb.next();
+
+        System.out.println(exp);
+
+
     }
 }
